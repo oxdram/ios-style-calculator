c14d1c499eb7a1890938da8f63d6bd8c4b52097d
oxdram <oxdramw@gmail.com>
2026-09-03 16:29:49 +0300

    Add color definitions for calculator UI theme

---
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val CalcBackground = Color(0xFF000000)
+val NumberButton = Color(0xFF333333)
+val NumberButtonPressed = Color(0xFF4D4D4D)
+val FunctionButton = Color(0xFFA5A5A5)
+val FunctionButtonPressed = Color(0xFFD9D9D9)
+val OperatorButton = Color(0xFFFF9F0A)
+val OperatorButtonPressed = Color(0xFFFFC069)
+val OperatorButtonActive = Color(0xFFFFFFFF)
+val TextWhite = Color(0xFFFFFFFF)
+val TextBlack = Color(0xFF000000)
