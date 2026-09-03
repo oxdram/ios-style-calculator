652be627f7929a41c0df5afa2aeec8ab1811e2e8
oxdram <oxdramw@gmail.com>
2026-09-03 16:30:20 +0300

    Add Typography definitions for app theme

---
+++ app/src/main/java/com/example/calculator/ui/theme/Type.kt
@@ -0,0 +1,17 @@
+package com.example.calculator.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontWeight = FontWeight.Light,
+        fontSize = 96.sp
+    ),
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    )
+)
